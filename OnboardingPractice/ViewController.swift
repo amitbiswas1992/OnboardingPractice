ecfd1fdde8c954245a217b466ca9c58b2acaef91
Amit K Biswas <biswas1992@hotmail.com>
2018-03-02 18:48:24 -0500

    Added onboarding Item Info

--- OnboardingPractice/ViewController.swift
+++ OnboardingPractice/ViewController.swift
@@ -7,8 +7,36 @@
 //
 
 import UIKit
+import PaperOnboarding
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, PaperOnboardingDataSource {
+   
+    func onboardingItemsCount() -> Int {
+    return 1
+    }
+    
+    func onboardingItem(at index: Int) -> OnboardingItemInfo {
+      
+        
+        let backgroundColorOne = UIColor(red: 217/255, green: 72/255,blue: 89/255, alpha: 1)
+        
+        let titleFont = UIFont(name: "AvenirNext-Bold", size: 24 )!
+        let descriptionFont = UIFont(name: "AvenirNext-Regular", size:18)!
+        
+        // return [(informationImage: #imageLiteral(resourceName: "Website"), title: "title", description: "description", iconName: UIImage , color: backgroundColorOne, titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)]
+        
+        return [OnboardingItemInfo(informationImage: #imageLiteral(resourceName: "rocket"),
+                                   title: "title",
+                                   description: "description",
+                                   pageIcon: #imageLiteral(resourceName: "BlueIcon"),
+                                   color: backgroundColorOne,
+                                   titleColor: UIColor.red,
+                                   descriptionColor: UIColor.orange,
+                                   titleFont: titleFont,
+                                   descriptionFont: descriptionFont)][index]
+        
+    }
+    
 
     
     @IBOutlet weak var onboardingView: UIView!
