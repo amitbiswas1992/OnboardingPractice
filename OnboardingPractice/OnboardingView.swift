4c781a19b7d5cd15f44f7d1b7712205b5624d1ee
Amit K Biswas <biswas1992@hotmail.com>
2018-03-02 17:50:24 -0500

    Initial commit

---
+++ OnboardingPractice/OnboardingView.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardingView.swift
+//  OnboardingPractice
+//
+//  Created by MazeGeek on 3/2/18.
+//  Copyright © 2018 Amit Biswas. All rights reserved.
+//
+
+import UIKit
+import PaperOnboarding
+
+class OnboardingView: PaperOnboarding {
+
+}
--- OnboardingPractice/ViewController.swift
+++ OnboardingPractice/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var onboardingView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
